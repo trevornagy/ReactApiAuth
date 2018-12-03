e2f2c54a9031969a84764a57a678f99dcf9d3513
Trevor Nagy <tnagy@nccinc.ca>
2018-12-02 23:05:13 -0500

    Added files for React Auth project.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./components/App";
+import Welcome from "./components/Welcome";
+import Feature from "./components/Feature";
+import Signup from "./components/auth/Signup";
+import Signin from "./components/auth/Signin";
+import Signout from "./components/auth/Signout";
+import reducers from "./reducers";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App>
+        <Route path="/" exact component={Welcome} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/signin" component={Signin} />
+        <Route path="/signout" component={Signout} />
+        <Route path="/feature" component={Feature} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector("#root")
+);
